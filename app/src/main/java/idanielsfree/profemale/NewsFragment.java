39cad413d5b67056ee88257d767269939ce131f9
IDanielsFree <ethicalsoftwares.app@gmail.com>
2019-05-08 13:55:54 -0300

    Second commit

--- app/src/main/java/idanielsfree/profemale/NewsFragment.java
+++ app/src/main/java/idanielsfree/profemale/NewsFragment.java
@@ -1,11 +1,18 @@
 package idanielsfree.profemale;
 
 
+import android.annotation.SuppressLint;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -13,17 +20,51 @@ import android.view.ViewGroup;
  */
 public class NewsFragment extends Fragment {
 
+    private RecyclerView staggeredRv;
+    private StaggeredRecyclerAdapter adapter;
+    private StaggeredGridLayoutManager manager;
+    private Context context;
 
     public NewsFragment() {
         // Required empty public constructor
     }
 
 
+    @SuppressLint("ResourceType")
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_news, container, false);
+
+        View rootView = inflater.inflate(R.layout.fragment_news, container, false);
+
+        staggeredRv = (RecyclerView) rootView.findViewById(R.id.staggered_rv_news);
+        manager = new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL);
+        staggeredRv.setLayoutManager(manager);
+
+        List<row> lst = new ArrayList<>();
+        lst.add(new row(R.drawable.img1));
+        lst.add(new row(R.drawable.img2));
+        lst.add(new row(R.drawable.img3));
+        lst.add(new row(R.drawable.img4));
+        lst.add(new row(R.drawable.img5));
+        lst.add(new row(R.drawable.img6));
+        lst.add(new row(R.drawable.img7));
+        lst.add(new row(R.drawable.img8));
+        lst.add(new row(R.drawable.img9));
+        lst.add(new row(R.drawable.img10));
+        lst.add(new row(R.drawable.img3));
+        lst.add(new row(R.drawable.img4));
+        lst.add(new row(R.drawable.img5));
+        lst.add(new row(R.drawable.img6));
+        lst.add(new row(R.drawable.img7));
+        lst.add(new row(R.drawable.img8));
+        lst.add(new row(R.drawable.img9));
+        lst.add(new row(R.drawable.img10));
+
+        adapter = new StaggeredRecyclerAdapter(getContext(), lst);
+        staggeredRv.setAdapter(adapter);
+
+        return rootView;
     }
 
 }
